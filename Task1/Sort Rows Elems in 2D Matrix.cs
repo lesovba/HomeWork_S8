706170254f2a0e2b47a1a08d1d0016e7ebb56b3a
Artem Golubev <lesovba@gmail.com>
2023-06-03 17:47:34 +0500

    Completed Task#1.

---
+++ Task1/Sort Rows Elems in 2D Matrix.cs
@@ -0,0 +1,65 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит 
+по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2*/
+
+int[,] array = GetArray(1, 9);
+SortRowsData(array);
+Console.WriteLine();
+
+//В первом методе задаем и выводим в консоль массив.
+int[,] GetArray(int minValue, int maxValue)
+
+{
+    Console.Clear();
+    Console.Write("Введите количество строк массива: ");
+    int rows = int.Parse(Console.ReadLine());
+
+    Console.Write("Введите количество столбцов массива: ");
+    int columns = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+            Console.Write($" {result[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    return result;
+}
+
+//Во втором методе сортируем данные в строках, согласно условию задачи, и выводим результат в консоль.  
+
+int[,] SortRowsData(int[,] arr)
+{
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < arr.GetLength(1); k++)
+            {
+                if (arr[i, j] < arr[i, k])
+                {
+                    int temp = arr[i, j];
+                    arr[i, j] = arr[i, k];
+                    arr[i, k] = temp;
+                }
+            }
+            Console.Write($" {arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    return arr;
+}
